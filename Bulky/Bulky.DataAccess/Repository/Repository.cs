3a33afdea26609b57c3726aa812640b23868f1e9
Dell <priyanshughatiya28@gmail.com>
2023-08-11 13:44:13 +0530

    BulkWebProject

---
+++ Bulky/Bulky.DataAccess/Repository/Repository.cs
@@ -0,0 +1,51 @@
+﻿using Bulky.DataAccess.Data;
+using Bulky.DataAccess.Repository.IRepository;
+using Microsoft.CodeAnalysis.CSharp.Syntax;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Linq.Expressions;
+
+namespace Bulky.DataAccess.Repository
+{
+    public class Repository<T> : IRepository<T> where T : class
+    {
+        private readonly ApplicationDbContext _db;
+        internal DbSet<T> dbSet;
+        public Repository( ApplicationDbContext db)
+        {
+            _db = db;
+            this.dbSet=_db.Set<T>();
+        }
+        public void Add(T enitity)
+        {
+            dbSet.Add(enitity);
+        }
+
+        public T Get(Expression<Func<T, bool>> filter)
+        {
+            IQueryable<T> query = dbSet;
+            query = query.Where(filter);
+            return query.FirstOrDefault();
+        }
+       public IEnumerable<T> GetAll() 
+        {
+            IQueryable<T> query = dbSet;
+            return query.ToList();
+        }
+
+        public void Remove(T entity)
+        {
+            dbSet.Remove(entity);
+        }
+
+        public void RemoveRange(IEnumerable<T> entity)
+        {
+            dbSet.RemoveRange(entity);
+        }
+       
+    }
+}
